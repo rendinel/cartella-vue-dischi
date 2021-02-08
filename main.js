53b765b953d7eaf8e77b5d52d02e719d7aaa6399
lucio rendine <luciorendine69@gmail.com>
2021-02-08 15:45:46 +0100

    milestone 1 ok

---
+++ main.js
@@ -0,0 +1,19 @@
+new Vue({
+  el: '#app',
+  data: {
+    disksList:[]
+  },
+  mounted() {
+  const self = this;
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then(function(resp){
+       const disks = resp.data.response
+       console.log(disks);
+       self.disksList = disks;
+    })
+  
+  },
+
+
+})
+Vue.config.devtools = true
